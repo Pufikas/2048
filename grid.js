3329793663a8d41679dcccb0259741e30f14f07e
Pufikas <dominykas.guzys@gmail.com>
2023-09-14 11:03:16 +0300

    tile

--- grid.js
+++ grid.js
@@ -3,12 +3,66 @@ const CELL_SIZE = 20;
 const CELL_GAP = 2;
 
 export default class Grid {
+    #cells; // set cells to private for grid
+    
     constructor(gridElement) {
         gridElement.style.setProperty("--grid-size", GRID_SIZE);
         gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`);
         gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`);
 
-        createCellElements(gridElement);
+        this.#cells = createCellElements(gridElement).map((cellElement, index) => {
+            return new Cell(cellElement, index % GRID_SIZE, Math.floor(index / GRID_SIZE));
+        });
+        console.log(this.cells)
+    }
+ 
+    get cellsByColumn() {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.x] = cellGrid[cell.x] || [];
+            cellGrid[cell.x][cell.y] = cell;
+            return cellGrid;
+        }, [])
+    }
+
+    get #emptyCells() {
+        return this.#cells.filter(cell => cell.tile == null);
+    }
+
+    randomEmptyCell() {
+        const randomIndex = Math.floor(Math.random() * this.#emptyCells.length);
+        return this.#emptyCells[randomIndex];
+    }
+}
+
+class Cell {
+    #cellElement;
+    #x;
+    #y;
+    #tile;
+
+    constructor(cellElement, x, y) {
+        this.#cellElement = cellElement;
+        this.#x = x;
+        this.#y = y;
+    }
+
+    get x() {
+        return this.#x;
+    }
+
+    get y() {
+        return this.#y;
+    }
+
+    get tile() {
+        return this.#tile;
+    }
+
+    set tile(value) {
+        this.#tile = value;
+        if (value == null) return;
+        this.#tile.x = this.#x;
+        this.#tile.y = this.#y;
     }
 }
 
@@ -17,6 +71,9 @@ function createCellElements(gridElement) {
 
     for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
         const cell = document.createElement("div");
-        cell  
+        cell.classList.add("cell");
+        cells.push(cell);
+        gridElement.append(cell);
     };
+    return cells;
 };
--- script.js
+++ script.js
@@ -1,5 +1,45 @@
 import Grid from "./grid.js";
+import Tile from "./tile.js";
 
 const gameBoard = document.getElementById("gameBoard");
 
 const grid = new Grid(gameBoard);
+
+setupInput();
+
+console.log(grid.cellsByColumn())
+console.log(grid.randomEmptyCell())
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+
+function setupInput() {
+    window.addEventListener("keydown", handleInput, { once: true });
+}
+
+function handleInput(e) {
+    console.log(e.key)
+    switch (e.key) {
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight":
+            moveRight();
+            break;
+        default:
+            setupInput();
+            return;
+    }
+    setupInput();
+}
+
+
+
+function moveUp() {
+    return slideTiles(grid.cellsByColumn);
+}
