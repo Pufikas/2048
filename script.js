2c4059f9820c1fa20aa90c5d6481ba08195b3a6b
Pufikas <dominykas.guzys@gmail.com>
2023-09-15 22:23:09 +0300

    game

--- script.js
+++ script.js
@@ -19,18 +19,40 @@ function setupInput() {
 async function handleInput(e) {
     console.log(e.key)
     switch (e.key) {
+
         case "ArrowUp":
+            if (!canMoveUp()) {
+                setupInput();
+                return;
+            }
+
             await moveUp();
             break;
+
         case "ArrowDown":
+            if (!canMoveDown()) {
+                setupInput();
+                return;
+            }
             await moveDown();
             break;
+
         case "ArrowLeft":
+            if (!canMoveLeft()) {
+                setupInput();
+                return;
+            }
             await moveLeft();
             break;
+
         case "ArrowRight":
+            if (!canMoveRight()) {
+                setupInput();
+                return;
+            }
             await moveRight();
             break;
+
         default:
             setupInput();
             return;
@@ -38,6 +60,9 @@ async function handleInput(e) {
     }
 
     grid.cells.forEach(cell => cell.mergeTiles());
+
+    const newTile = new Tile(gameBoard);
+    grid.randomEmptyCell().tile = newTile;
 
     setupInput();
 }
@@ -58,6 +83,39 @@ async function handleInput(e) {
     function moveRight() {
         return slideTiles(grid.cellsByRow.map(row => [...row].reverse()));
     }
+
+    // controls can move up...
+
+    function canMoveUp() {
+        return canMove(grid.cellsByColumn);
+    }
+
+    function canMoveDown() {
+        return canMove(grid.cellsByColumn.map(column => [...column].reverse()));
+    }
+
+    function canMoveLeft() {
+        return canMove(grid.cellsByRow);
+    }
+
+    function canMoveRight() {
+        return canMove(grid.cellsByRow.map(row => [...row].reverse()));
+    }
+
+    function canMove(cells) {
+        return cells.some(group => {
+            return group.some((cell, index) => {
+                if (index === 0) return false;
+                if (cell.tile == null) return false;
+
+                const moveToCell = group[index - 1];
+
+                return moveToCell.canAccept(cell.tile);
+            })
+        })
+    }
+
+    // controls end
 // controls end
 
 function slideTiles(cells) {
